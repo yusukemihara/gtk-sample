03fd6885b0675dfe0c606fcdfe0948108e30ff12
Jck Frst <mihara@lucid45.(none)>
2011-12-02 13:43:12 +0900

    fix cosme

--- testnotebook.c
+++ testnotebook.c
@@ -18,25 +18,25 @@ cb_switch_page(
   guint pageno,
   gpointer data)
 {
-	printf("switch page [%d][%p]\n",pageno,page);
+  printf("switch page [%d][%p]\n",pageno,page);
 }
 
 int
 main(int argc, char *argv[])
 {
-	GtkBuilder *builder;
-	GObject *window1;
-	GObject *entry1;
+  GtkBuilder *builder;
+  GObject *window1;
+  GObject *entry1;
 
-	gtk_init(&argc,&argv);
+  gtk_init(&argc,&argv);
 
-	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder, "testnotebook.ui", NULL);
-	gtk_builder_connect_signals(builder, NULL);
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "testnotebook.ui", NULL);
+  gtk_builder_connect_signals(builder, NULL);
 
-	window1 = gtk_builder_get_object(builder, "window1");
-	gtk_widget_show_all(GTK_WIDGET(window1));
-	gtk_main();
+  window1 = gtk_builder_get_object(builder, "window1");
+  gtk_widget_show_all(GTK_WIDGET(window1));
+  gtk_main();
 
-	return 0;
+  return 0;
 }
